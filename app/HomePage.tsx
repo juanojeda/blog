81814099c929f5c0a63dd30846ce777da6b5d084
juanojeda <juan@juanojeda.com>
2025-03-21 20:31:48 +1100

    create a basic text component

--- app/HomePage.tsx
+++ app/HomePage.tsx
@@ -1,17 +1,18 @@
 'use client'
+import Text from '@/components/Text/Text';
 import Link from 'next/link';
  
 export default function HomePage({ posts }) {
   return (
     <div>
-      <h1>My Blog</h1>
+      <Text as="h1">My Blog</Text>
       <ul>
         {posts.map((post) => (
           <li key={post.slug}>
             <Link href={`/posts/${post.slug}`}>
               {post.title}
             </Link>
-            <p>{post.date}</p>
+            <Text>{post.date}</Text>
           </li>
         ))}
       </ul>
