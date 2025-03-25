716cac69280f7137fccaeffd1d51f0409f2ca160
juanojeda <juan@juanojeda.com>
2025-03-25 15:07:32 +1100

    layout tweaks to post

--- src/app/posts/[slug]/PostLayout.tsx
+++ src/app/posts/[slug]/PostLayout.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Box, Link, Paper } from "@mui/material";
+import { Box, Grid2 as Grid, Link, Paper } from "@mui/material";
 import Typography from "@mui/material/Typography";
 
 const PostLayout = ({ frontmatter, children }: { frontmatter: any; children: React.ReactNode }) => {
@@ -7,10 +7,14 @@ const PostLayout = ({ frontmatter, children }: { frontmatter: any; children: Rea
     <>
       <Link href="/"> Back home</Link>
       <Box component={'article'}>
-        <Box sx={{  mb: 4 }}>
-          <Typography variant="h1">{frontmatter.title}</Typography>
-          <Typography variant="body1">Posted on {frontmatter.date}</Typography>
-        </Box>
+        <Grid sx={{px: 0, py: 3}} container justifyContent={'space-between'} alignItems={'flex-end'}>
+          <Grid>
+            <Typography sx={{mb: 0}} variant="h1">{frontmatter.title}</Typography>
+          </Grid>
+          <Grid>
+            <Typography variant="body1">Posted on {frontmatter.date}</Typography>
+          </Grid>
+        </Grid>
         <Paper sx={{
           py:4,
           px:4,
