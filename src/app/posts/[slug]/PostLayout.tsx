f8b51e0163f9d57e0fcdceb008682688c3598064
juanojeda <juan@juanojeda.com>
2025-03-25 15:00:22 +1100

    add home link on post layout

--- src/app/posts/[slug]/PostLayout.tsx
+++ src/app/posts/[slug]/PostLayout.tsx
@@ -1,21 +1,25 @@
 "use client";
-import { Box, Paper } from "@mui/material";
+import { Box, Link, Paper } from "@mui/material";
 import Typography from "@mui/material/Typography";
 
 const PostLayout = ({ frontmatter, children }: { frontmatter: any; children: React.ReactNode }) => {
   return (
-    <Box component={'article'}>
-      <Box sx={{  mb: 4 }}>
-        <Typography variant="h1">{frontmatter.title}</Typography>
-        <Typography variant="body1">Posted on {frontmatter.date}</Typography>
+    <>
+      <Link href="/"> Back home</Link>
+      <Box component={'article'}>
+        <Box sx={{  mb: 4 }}>
+          <Typography variant="h1">{frontmatter.title}</Typography>
+          <Typography variant="body1">Posted on {frontmatter.date}</Typography>
+        </Box>
+        <Paper sx={{
+          py:4,
+          px:4,
+        }} elevation={0} >
+          {children}
+        </Paper>
       </Box>
-      <Paper sx={{
-        py:4,
-        px:4,
-      }} elevation={0} >
-        {children}
-      </Paper>
-    </Box>
+    
+    </>
   );
 };
 
