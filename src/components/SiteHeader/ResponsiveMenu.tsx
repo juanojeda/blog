7ee851534de7576429202ed28dfa2098c3df18ab
juanojeda <juan@juanojeda.com>
2025-04-08 09:30:42 +1000

    refine design of mobile menu

--- src/components/SiteHeader/ResponsiveMenu.tsx
+++ src/components/SiteHeader/ResponsiveMenu.tsx
@@ -22,7 +22,7 @@ const ResponsiveMenu: React.FC<ResponsiveMenuProps> = ({ navItems }) => {
   };
 
   return (
-    <Box role="presentation">
+    <Box>
       <IconButton
         color='inherit'
         aria-label="open drawer"
@@ -35,18 +35,17 @@ const ResponsiveMenu: React.FC<ResponsiveMenuProps> = ({ navItems }) => {
         <MenuIcon />
       </IconButton>
       <Box component="nav" sx={{ display: { xs: 'block', sm: 'none' } }}>
-        <Drawer anchor='right' variant="temporary" open={mobileOpen} onClose={handleDrawerToggle} ModalProps={{ keepMounted: true }} sx={{
-          display: { xs: 'block', sm: 'none' }
+        <Drawer anchor='top' variant="temporary" open={mobileOpen} onClose={handleDrawerToggle} ModalProps={{ keepMounted: true }} sx={{
+          display: { xs: 'block', sm: 'none' },
+          '& .MuiDrawer-paper': { boxSizing: 'border-box', width: 'auto', backgroundColor: 'primary.dark' }
         }}>
-          <Box onClick={handleDrawerToggle} sx={{ textAlign: 'left' }}>
-            <List>
-              {navItems.map((item) => (
-                <ListItem key={item.name} disablePadding>
-                  <Link href={item.path} variant='h5'>{item.name}</Link>
-                </ListItem>
-              ))}
-            </List>
-          </Box>
+          <List sx={{ textAlign: 'left', py: 4, px: 2 }}>
+            {navItems.map((item) => (
+              <ListItem key={item.name}>
+                <Link href={item.path} variant='h5' color="primary.contrastText">{item.name}</Link>
+              </ListItem>
+            ))}
+          </List>
         </Drawer>
       </Box>
     </Box>
